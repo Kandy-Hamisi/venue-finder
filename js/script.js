81b59e01ab388d6382dd6d390733c1a62fd00ba5
martin thuo <mertoenjosh3527@gmail.com>
2021-03-29 00:20:03 +0300

    need to add an input form tomorrow

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.book');
+const btnsOpenModal = document.querySelectorAll('.btn-book');
 
 const openModal = function() {
     modal.classList.remove('hidden');
